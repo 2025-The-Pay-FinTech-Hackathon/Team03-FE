4d4930f29ff3e062b52a035c1776f9d5a65520f0
Son Ahyun <dkgus731@naver.com>
2025-05-16 00:17:56 +0900

    ✨ VerticalActionButton 컴포넌트 추가

---
+++ src/components/Button/VerticalActionButton.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+interface VerticalActionButtonProps {
+  children: React.ReactNode;
+  onClick: () => void;
+  disabled?: boolean;
+}
+
+const VerticalActionButton: React.FC<VerticalActionButtonProps> = ({
+  children,
+  onClick,
+  disabled = false,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={`
+        w-fit
+        p-4
+        flex
+        flex-col
+        items-center
+        justify-center
+        rounded-full
+        font-semibold
+        text-base
+        transition-all
+        duration-200
+        ${
+          !disabled &&
+          `
+          hover:scale-102
+          hover:bg-[#FAF6F6]
+          active:scale-98
+          cursor-pointer
+        `
+        }
+        disabled:cursor-not-allowed
+        disabled:transform-none
+        disabled:shadow-none
+      `}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default VerticalActionButton;
