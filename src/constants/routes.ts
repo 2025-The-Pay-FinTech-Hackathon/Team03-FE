2870f7331f74c9caf01552bd2c357e94d7b36f01
Son Ahyun <dkgus731@naver.com>
2025-05-15 22:19:16 +0900

    🔧 ROUTES 상수에 퀘스트 생성 페이지 추가

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -3,7 +3,8 @@ export const ROUTES = {
   signup: "/signup", // 회원가입
   onBoarding: "/onboarding", // 온보딩
   home: "/", // 홈 화면
-  quest: "/quest", // 퀘스트페이지
+  quest: "/quest", // 퀘스트 페이지
+  createQuest: "/quest/create", // 퀘스트 생성 페이지
   questDetail: (questId: string) => `/quest/${questId}`, // 퀘스트 상세페이지
   limit: "/limit", // 소비제한 페이지
   report: "/report", // 소비리포트 페이지
