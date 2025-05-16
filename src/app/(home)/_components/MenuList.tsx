649983c738c0f1d967911674df89ec06ee4c6b3e
Son Ahyun <dkgus731@naver.com>
2025-05-16 17:37:54 +0900

    ✨ MenuList 컴포넌트에서 버튼 클릭 시 콘솔 로그 대신 라우터를 사용하여 페이지 이동 기능 추가

--- src/app/(home)/_components/MenuList.tsx
+++ src/app/(home)/_components/MenuList.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import VerticalActionButton from "@/components/Button/VerticalActionButton";
+import { ROUTES } from "@/constants/routes";
 
 export default function MenuList() {
+  const router = useRouter();
+
   return (
     <div className="flex justify-between items-center gap-4 bg-white rounded-2xl px-8 py-4">
-      <VerticalActionButton onClick={() => console.log("소비 제한 클릭")}>
+      <VerticalActionButton onClick={() => router.push(ROUTES.limit)}>
         <Image
           src="/icons/menu/limit.png"
           alt="소비 제한"
@@ -16,7 +20,7 @@ export default function MenuList() {
         <span className="text-sm">소비 제한</span>
       </VerticalActionButton>
 
-      <VerticalActionButton onClick={() => console.log("퀘스트 클릭")}>
+      <VerticalActionButton onClick={() => router.push(ROUTES.quest)}>
         <Image
           src="/icons/menu/quest.png"
           alt="퀘스트"
@@ -26,7 +30,7 @@ export default function MenuList() {
         <span className="text-sm">퀘스트</span>
       </VerticalActionButton>
 
-      <VerticalActionButton onClick={() => console.log("AI 소비 리포트 클릭")}>
+      <VerticalActionButton onClick={() => router.push(ROUTES.aiReport)}>
         <Image
           src="/icons/menu/aiReport.png"
           alt="AI 분석 소비 리포트"
@@ -36,7 +40,7 @@ export default function MenuList() {
         <span className="text-sm">AI 소비 분석</span>
       </VerticalActionButton>
 
-      <VerticalActionButton onClick={() => console.log("설정 클릭")}>
+      <VerticalActionButton onClick={() => router.push(ROUTES.setting)}>
         <Image
           src="/icons/menu/setting.png"
           alt="설정"
