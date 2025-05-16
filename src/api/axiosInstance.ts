12d9a5cd59cd1e769c7739fc4819346ecd325c9a
Son Ahyun <dkgus731@naver.com>
2025-05-16 13:45:24 +0900

    ✨ axios 및 @types/axios 패키지 추가와 관련된 의존성 업데이트

---
+++ src/api/axiosInstance.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
