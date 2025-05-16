8002eae583da7e114f6019a2ce84bb42bb76bb41
Son Ahyun <dkgus731@naver.com>
2025-05-16 13:52:09 +0900

    ✨ 새로운 제한 카테고리 상수 파일 추가

---
+++ src/constants/limitCategory.ts
@@ -0,0 +1,37 @@
+export const LIMIT_CATEGORIES = [
+  {
+    key: "GAME",
+    name: "게임",
+    image: "/icons/limitCategory/GAME.png",
+  },
+  {
+    key: "GAMBLING",
+    name: "도박",
+    image: "/icons/limitCategory/GAMBLING.png",
+  },
+  {
+    key: "ALCOHOL",
+    name: "주류/담배",
+    image: "/icons/limitCategory/ALCOHOL.png",
+  },
+  {
+    key: "ADULT",
+    name: "성인/유흥",
+    image: "/icons/limitCategory/ADULT.png",
+  },
+  {
+    key: "LUXURY",
+    name: "명품",
+    image: "/icons/limitCategory/LUXURY.png",
+  },
+  {
+    key: "DELIVERY",
+    name: "배달음식",
+    image: "/icons/limitCategory/DELIVERY.png",
+  },
+  {
+    key: "SHOPPING",
+    name: "온라인쇼핑",
+    image: "/icons/limitCategory/SHOPPING.png",
+  },
+];
