70e409de092dafe980a73c32fce154029acab5ff
Son Ahyun <dkgus731@naver.com>
2025-05-16 13:50:02 +0900

    ✨ 퀘스트 카테고리 아이콘 및 상수 파일 추가

---
+++ src/constants/questCategory.ts
@@ -0,0 +1,27 @@
+export const QUEST_CATEGORIES = [
+  {
+    key: "HOUSEWORK",
+    name: "집안일",
+    image: "/icons/questCategory/HOUSEWORK.png",
+  },
+  {
+    key: "STUDY",
+    name: "공부",
+    image: "/icons/questCategory/STUDY.png",
+  },
+  {
+    key: "ERRAND",
+    name: "심부름",
+    image: "/icons/questCategory/ERRAND.png",
+  },
+  {
+    key: "SELFCARE",
+    name: "자기관리",
+    image: "/icons/questCategory/SELFCARE.png",
+  },
+  {
+    key: "ETC",
+    name: "기타",
+    image: "/icons/questCategory/ETC.png",
+  },
+];
