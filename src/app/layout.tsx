1ed93bcd473e1108aa557fe5ae095ff8438427e9
Son Ahyun <dkgus731@naver.com>
2025-05-17 06:04:58 +0900

    ✨ PWA 지원을 위한 설정 추가: next-pwa 패키지 통합 및 서비스 워커 파일 추가, 웹 앱 매니페스트 및 아이콘 파일 구성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,23 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <head>
-        <title>Team03</title>
+        <link rel="manifest" href="./manifest.webmanifest" />
+        <title>용돈의 숲</title>
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="apple-mobile-web-app-title" content="ClassLog" />
+        <link
+          rel="apple-touch-icon"
+          href="/favicon/apple-icon.png"
+          sizes="180x180"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
       </head>
       <body className="antialiased min-h-screen bg-white overflow-hidden">
         <div className="mx-auto max-w-[600px] min-h-screen bg-[#f7f8f8] shadow-lg color-[#1E1E1E] h-full relative">
